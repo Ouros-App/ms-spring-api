1078d1d1efeb0bd74ae73bd5d760a309b8bf2da5
Ouros <ouros.app@gmail.com>
2026-08-14 12:35:33 -0300

    Add Spring REST scaffold: SpringApiApplicationTests.java

---
+++ src/test/java/com/ourosapp/springapi/SpringApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.ourosapp.springapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
